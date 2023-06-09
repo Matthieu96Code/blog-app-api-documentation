f973a027db769fa094023c0c3cab99c908a5ec1b
mumo-esther <mumoesther2021@gmail.com>
2023-06-09 07:47:24 +0300

    Implement the references method to create foreign key columns author_id and post_id,

--- db/migrate/20230301213058_create_comments.rb
+++ db/migrate/20230301213058_create_comments.rb
@@ -1,11 +1,16 @@
-class CreateComments < ActiveRecord::Migration[7.0]
+class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :AuthorId
-      t.integer :PostId
-      t.text :Text
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: true
+      t.text :text
+      t.datetime :updated_at
+      t.datetime :created_at
 
       t.timestamps
     end
+
+    add_index :comments, :author_id
+    add_index :comments, :post_id
   end
 end
