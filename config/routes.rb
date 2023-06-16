9c69c959954c9345675da8c52408eb742b59e912
mumo-esther <mumoesther2021@gmail.com>
2023-06-16 16:29:44 +0300

    Update the comments resource to accommodate your newly created new method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
     root "users#index"
-    
+  
     resources :users, only: [:index, :show] do
       resources :posts, only: [:index, :new, :create, :show] do
-       resources :comments, only: [:create]
+        resources :comments, only: [:new, :create]
         resources :likes, only: [:create]
+      end
     end
   end
-end
+  
