8a268de9e3af34c9d3883f6cf9c804e660baed4f
mumo-esther <mumoesther2021@gmail.com>
2023-06-22 11:32:30 +0300

    Add routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
     root "users#index"
-  
+    
     resources :users, only: [:index, :show] do
       resources :posts, only: [:index, :new, :create, :show] do
-
        resources :comments, only: [:create]
         resources :likes, only: [:create]
     end
   end
 end
-
-       
