f158fa03ba2a98b7a4bc54e263fa7616f1417adb
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-23 09:46:36 +0000

    Update application  with cancancan config

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative "boot"
-
+require "cancancan"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,7 +10,7 @@ module BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
